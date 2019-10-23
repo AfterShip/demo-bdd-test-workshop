73fe0fdc89e150ac0e186ce61959c167a5f3b3f1
nonjene <reanfn89@gmail.com>
2019-10-24 00:25:44 +0800

    update step

--- cypress/integration/login_aftership/login_aftership.js
+++ cypress/integration/login_aftership/login_aftership.js
@@ -2,27 +2,27 @@ Given("I am a user, I never login to Aftership site before", () => {
   // ok I am a user, nothing to need to be initialized
 });
 
-When(/I visit "(.+)"/, url => {
+When("I visit {string}", url => {
   cy.visit(url);
 });
 
-Then(/I can see "(.+)"/, someText => {
+Then("I can see {string}", someText => {
   cy.contains(someText);
 });
 
-And(/I can see a form with these fields/, ({ rawTable }) => {
+And("I can see a form with these fields:", ({ rawTable }) => {
   for (const td of rawTable[0]) {
     cy.get(`input[name='${td}']`);
   }
 });
 
-When(/I type in these values to the form/, ({ rawTable }) => {
+When("I type in these values to the form:", ({ rawTable }) => {
   for (const tr of rawTable) {
     cy.get(`input[name='${tr[0]}']`).type(tr[1]);
   }
 });
 
-And(/I click the "(.+)" button/, buttonName => {
+And("I click the {string} button", buttonName => {
   cy.get("button")
     .contains(buttonName)
     .click();
